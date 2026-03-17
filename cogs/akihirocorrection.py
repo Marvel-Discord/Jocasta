5c29f65929fd73d66e7039ec5cb8d04cecb58b85
BowGaming <109923007+BowGaming@users.noreply.github.com>
2026-03-17 12:48:39 +0100

    Update akihirocorrection.py
    Updated such that the use cog works in all channels and forums where @ everyone has permissions to talk.

--- cogs/akihirocorrection.py
+++ cogs/akihirocorrection.py
@@ -12,65 +12,66 @@ class AkihiroCog(commands.Cog) :
         self.akihiro_message = Embed(
             description=(
                 "Please do not use the name 'Daken' as this is actually a slur.\n"
-                "Instead, use one of these alternatives: Akihiro, Dark Wolverine, Hellverine, Fang."
+                "Instead, use alternatives such as Akihiro, Dark Wolverine, Hellverine or Fang."
             ),
         )
 
-        # Ignored channels and forums
-        self.ignored_channels = {
-          1109693299297632328, # rules
-          1110214786223968346, # announcements
-          1109021470736252938, # comic news
-          1109021386015522816, # movie tv news
-          1109021556371378239, # misc news
-          1109728410894356500, # server affiliates
-          1109722494002286642, # resource server info
-          1109722813058777148, # resource faq
-          1109723001836011621, # resource contact staff
-          1109723183612964864, # resource ban appeals
-          1109723343814406224, # resource spoiler archive
-          1109723474177560647, # resource activities
-          1108985848558534789, # staff room
-          1328965818490294332, # manager room
-          1328709586554851359, # mod room
-          1109011915402903612, # staff log
-          1108992635957416016, # staff bot commands
-          1109033106431811664, # automod log
-          1207619588565499924, # murderworld
-          1382732564610945194, # comic review corner
-          1362621880779014397, # bookclub organising (thread)
-          1109032560471842826, # polls
-          1109032581233659904, # special polls
-          1214133803107491840, # polls archive
-          1109023403060506634, # polls management
-          1252175705480106015, # polls contributors
-          1109726177964331148, # tykhe announcements
-          1109726240803405894, # tykhe shop
-          1109726292208779385, # tykhe rolling
-          1110217807116906507, # tykhe rolling 2
-          1111142854186762271, # tykhe roll nft
-          1109727103722729573, # tykhe commands
-          1110217858446790656, # tykhe commands 2
-          1111141979376582729, # tykhe trade board
-          1109727127051440148, # tykhe discussion
-          1108993540463284274, # carl logs
-          1110536802638512239, # member log
-          1119123107848933416, # carl archive log
-          1264919405071040593, # spoiler filter log
-          1475009747693342770, # kiai log
-          1478668829700657253, # userbot log
+        # Manual overrides for blocking/allowing channels
+        self.blocked_channels = {}
+        self.allowed_channels = {
+            1382732564610945194, # comic review corner
+            1362621880779014397, # bookclub organising (thread)
+            1109726292208779385, # tykhe rolling
+            1110217807116906507, # tykhe rolling 2
+            1111142854186762271, # tykhe roll nft
+            1109727103722729573, # tykhe commands
+            1110217858446790656, # tykhe commands 2
+            1111141979376582729, # tykhe trade board
+            1109727127051440148, # tykhe discussion
         }
-      
-        self.ignored_forums = {
-          1329352004224680040, # manager forum
-          1109026283331014677, # staff forum
-        }
+        self.blocked_forums = {}
+        self.allowed_forums = {}
 
         # Cooldown duration in seconds
         self.cooldown_seconds = 60
         self.last_executed = {}
 
-
+    def everyone_can_talk(self, ch, everyone_role):
+        overwrite = ch.overwrites_for(everyone_role)
+        
+        return (
+            overwrite.send_messages is not False and
+            overwrite.send_messages_in_threads is not False
+        )
+    
+    def is_blocked_channel(self, channel):
+        guild = channel.guild
+    
+        # Manual allowlist
+        if channel.id in self.allowed_channels:
+            return False
+        if isinstance(channel, Thread) and channel.parent and channel.parent.id in self.allowed_forums:
+            return False
+    
+        # Manual blocklist
+        if channel.id in self.blocked_channels:
+            return True
+        if isinstance(channel, Thread) and channel.parent and channel.parent.id in self.blocked_forums:
+            return True
+        
+        # @everyone permission check
+        everyone_role = guild.default_role
+        
+        if isinstance(channel, Thread):
+            parent = channel.parent
+            if parent and self.everyone_can_talk(parent, everyone_role):
+                return True
+        else:
+            if self.everyone_can_talk(channel, everyone_role):
+                return True
+    
+        return False
+    
     @commands.Cog.listener()
     async def on_message(self, message) :
         """Checks messages in the review channel and enforces format."""
@@ -78,27 +79,22 @@ class AkihiroCog(commands.Cog) :
             return
 
         # Ignore normal channels
-        if isinstance(message.channel, TextChannel):
-            if message.channel.id in self.ignored_channels:
-                return
-        # Ignore threads inside forums
-        if isinstance(message.channel, Thread):
-            if message.channel.parent and message.channel.parent.id in self.ignored_forums:
-                return
+        if not self.is_blocked_channel(message.channel):
+            return
             
         if "daken" not in message.content.lower():
             return
 
         channel_id = message.channel.id
-
+    
         # cooldown check
         now = time.time()
         last_time = self.last_executed.get(channel_id, 0)
         if now - last_time < self.cooldown_seconds:
             return  # still in cooldown
-
+    
         await message.reply(embed=self.akihiro_message)
-
+    
         # update last time used
         self.last_executed[channel_id] = now
         
