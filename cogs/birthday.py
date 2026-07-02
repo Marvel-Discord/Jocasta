24806f096b16b6e93256525f20233d8607d57417
Julian Invictus <35577027+JulianInvictus@users.noreply.github.com>
2026-07-02 09:03:45 -0400

    Refactor birthday role management and startup logic

--- cogs/birthday.py
+++ cogs/birthday.py
@@ -14,16 +14,33 @@ class BirthdayCog(commands.Cog, name="Birthday"):
     def __init__(self, bot):
         self.bot = bot
         self.bot.tasks["birthday"] = {}
+        self.bot.loop.create_task(self.on_startup())
 
-    async def _remove_role_after_delay(self, member: discord.Member, role: discord.Role):
-        await asyncio.sleep(BIRTHDAY_DURATION)
+    async def on_startup(self):
+        await self.bot.wait_until_ready()
+        guild = self.bot.get_guild(homeserver)
+        role = guild.get_role(BIRTHDAY_ROLE_ID)
+        if role is None:
+            return
+
+        for member in role.members:
+            if member.id not in self.bot.tasks["birthday"]:
+                task = asyncio.create_task(self._remove_role_after_delay(member.id))
+                self.bot.tasks["birthday"][member.id] = task
+
+    async def _remove_role_after_delay(self, member_id: int, delay: float = BIRTHDAY_DURATION):
+        await asyncio.sleep(delay)
+        guild = self.bot.get_guild(homeserver)
         try:
-            if role in member.roles:
-                await member.remove_roles(role, reason="Birthday role expired after 24 hours")
+            member = guild.get_member(member_id)
+            if member:
+                role = guild.get_role(BIRTHDAY_ROLE_ID)
+                if role and role in member.roles:
+                    await member.remove_roles(role, reason="Birthday role expired after 24 hours")
         except discord.HTTPException:
             pass
         finally:
-            self.bot.tasks.get("birthday", {}).pop(member.id, None)
+            self.bot.tasks.get("birthday", {}).pop(member_id, None)
 
     @app_commands.command(name="birthday")
     @app_commands.describe(member="The member whose birthday it is")
@@ -43,7 +60,7 @@ class BirthdayCog(commands.Cog, name="Birthday"):
                 f"{member.mention} already has an active birthday timer running.", ephemeral=True
             )
         elif existing:
-            del self.bot.tasks["birthday"][member.id]
+            self.bot.tasks["birthday"].pop(member.id, None)
 
         if role in member.roles:
             return await interaction.response.send_message(
@@ -51,7 +68,7 @@ class BirthdayCog(commands.Cog, name="Birthday"):
             )
 
         await member.add_roles(role, reason=f"Birthday assigned by {interaction.user} ({interaction.user.id})")
-        task = asyncio.create_task(self._remove_role_after_delay(member, role))
+        task = asyncio.create_task(self._remove_role_after_delay(member.id))
         self.bot.tasks["birthday"][member.id] = task
 
         await interaction.response.send_message(
@@ -64,8 +81,11 @@ class BirthdayCog(commands.Cog, name="Birthday"):
             msg = "You don't have permission to use this command."
         else:
             msg = "Something went wrong."
-        responder = interaction.followup.send if interaction.response.is_done() else interaction.response.send_message
-        await responder(msg, ephemeral=True)
+
+        if interaction.response.is_done():
+            await interaction.followup.send(msg, ephemeral=True)
+        else:
+            await interaction.response.send_message(msg, ephemeral=True)
 
     async def cog_unload(self):
         for task in self.bot.tasks.get("birthday", {}).values():
